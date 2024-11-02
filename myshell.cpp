d6dda35b1bec0b5bb7cef8d0a4d2e232d0cb6112
Vasant19 <vasantdave2002@gmail.com>
2024-11-02 16:36:08 -0400

    Added Simple shell program, Documented for explaination

--- myshell.cpp
+++ myshell.cpp
@@ -5,9 +5,9 @@ int main() {
     while (true) {
         cout << "myshell> "; // Display prompt
         string input; // Variable to hold user input
-        getline(std::cin, input); // Read user input
+        getline(cin, input); // Read user input
 
-        if (input == "exit" || input == "quit") {
+        if (input == "exit" || input == "quit" || input == "q") {
             break; // Exit the shell if user types exit or quit
         }
     
